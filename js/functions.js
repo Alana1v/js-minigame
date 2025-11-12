bd22720812cbfd17e3810674f10e7773e2f9a7e1
Marcelo Júnior <rokermarcelo@gmail.com>
2025-11-12 14:28:30 -0300

    Criada interface do minigame; refatoras funções JS para manter as funções "comuns" em um arquivo próprio

--- js/functions.js
+++ js/functions.js
@@ -1,110 +1,46 @@
-let time = 500
-let timer = setInterval(loop, time);
+let time = 500;
+let timer = null;
 let direction = null;
-let snake = null;
-let piece = null;
-let pieceOn = false;
 
+btn_top.onclick = () => {direction = 38};
+btn_left.onclick = () => {direction = 37};
+btn_right.onclick = () => {direction = 39};
+btn_botton.onclick = () => {direction = 40};
+btn_start.onclick = startGame;
+btn_end.onclick = endGame;
 document.body.onkeydown = function (e) {
-  if(e.which >= 37 && e.which <=40)
+  if(e.which >= 37 && e.which <=40){ // Teclas direcionais
     direction = e.which;
+  } else if(e.which == 32){ // Barra de espaço
+    startGame();
+  } else if(e.which == 19){ // Botão de pause
+    endGame();
+  }
 };
 
-start(); // Executa a função que inicia as variáveis
-
-function start(){
-  head.style.left = 0;
-  head.style.top = 0;
-  direction = 39; // para direita
-  snake = [head];
+// (Re)Inicia o timer do jogo e chama a função start()
+function startGame(){
+  if(timer != null) endGame();
+  Array.from(field.children).forEach(function(p){p.remove();});
+  time = 500;
+  timer = setInterval(loop, time);
+  start(); // Tem que ser criada no sou próprio Javascript
 }
 
-function loop(){
-  gameOver();
-  createPiece();
-  getPiece();
-  refresh();
-  move();
+function endGame(){
+  clearInterval(timer);
+  alert("Game Over");
+  timer = null;
+  return true;
 }
 
-function move(){
-  switch (direction) {
-    case 37: // Esquerda
-      snake[0].style.left = (getPosition(snake[0], "left")-20) + "px";
-      break;
-    case 38: // Cima
-      snake[0].style.top = (getPosition(snake[0], "top")-20) + "px";
-      break;
-    case 39: // Direita
-      snake[0].style.left = (getPosition(snake[0], "left")+20) + "px";
-      break;
-    case 40: // Baixo
-      snake[0].style.top = (getPosition(snake[0], "top")+20) + "px";
-      break;
-  }
-}
-
-function createPiece(){
-  if(!pieceOn){
-    let left = Math.floor(Math.random() * 9) * 20;
-    let top = Math.floor(Math.random() * 9) * 40;
+function newPiece(left, top){
     piece = document.createElement("div");
     piece.className = "piece";
-    piece.id = snake.length
     piece.style.left = left+"px";
     piece.style.top = top+"px";
-    field.insertBefore(piece, snake[0]);
-    pieceOn = true;
-  }
-}
-
-function gameOver(){
-  let left = getPosition(snake[0], "left");
-  let top = getPosition(snake[0], "top");
-  if(left < 0 || left > 180 || top < 0 || top > 380){
-    clearInterval(timer);
-    alert("Game Over");
-  }
-  else{
-    snake.forEach(function(piece, index, array){
-      if(index > 1){
-        if(colision(snake[0], piece)){
-          clearInterval(timer);
-          alert("Game Over");
-        }
-      }
-    });
-  }
-}
-
-function getPiece(){
-  if(colision(snake[0], piece)){
-    pieceOn = false;
-    // $(piece).remove();
-    piece.className = "piece tail";
-    snake.push(piece);
-
-    clearInterval(timer);
-    time -= 10
-    timer = setInterval(loop, time);
-  }
-}
-
-function refreshTail(){
-  snake.forEach(function(piece, index, array){
-    if(index > 0){
-      if(array.length > index+1){
-        piece.style.left = getPosition(array[index+1], "left")+"px";
-        piece.style.top = getPosition(array[index+1], "top")+"px";
-        // $(piece).css({left: getPosition(array[index+1], "left")+"px", top: getPosition(array[index+1], "top")+"px"})
-      }
-      else{
-        piece.style.left = getPosition(snake[0], "left")+"px";
-        piece.style.top = getPosition(snake[0], "top")+"px";
-        // $(piece).css({left: getPosition(snake[0], "left")+"px", top: getPosition(snake[0], "top")+"px"})
-      }
-    }
-  });
+    field.appendChild(piece);  
+    return piece;
 }
 
 function colision(objA, objB){
--- js/snake.js
+++ js/snake.js
@@ -1,89 +1,76 @@
-let time = 500
-let timer = setInterval(loop, time);
-let direction = null;
 let snake = null;
 let piece = null;
 let pieceOn = false;
 
-document.body.onkeydown = function (e) {
-  if(e.which >= 37 && e.which <=40)
-    direction = e.which;
-};
-
-start(); // Executa a função que inicia as variáveis
+nome_jogo.innerText = "Cobrinha";
 
 function start(){
-  head.style.left = 0;
-  head.style.top = 0;
+  head = newPiece(0, 0); // Cria peça na posição (0,0)
+  head.id = "head";
   direction = 39; // para direita
   snake = [head];
+  pieceOn = false;
+  createRandomPiece(Math.floor(Math.random() * 9) * 20, Math.floor(Math.random() * 9) * 40);
 }
 
 function loop(){
-  gameOver();
-  createPiece();
-  getPiece();
+  createRandomPiece(Math.floor(Math.random() * 9) * 20, Math.floor(Math.random() * 9) * 40);
+  eatPiece();
   refreshTail();
   move();
+  if(gameOver()) return;
 }
 
 function move(){
   switch (direction) {
     case 37: // Esquerda
-      snake[0].style.left = (getPosition(snake[0], "left")-20) + "px";
+      head.style.left = (getPosition(head, "left")-20) + "px";
       break;
     case 38: // Cima
-      snake[0].style.top = (getPosition(snake[0], "top")-20) + "px";
+      head.style.top = (getPosition(head, "top")-20) + "px";
       break;
     case 39: // Direita
-      snake[0].style.left = (getPosition(snake[0], "left")+20) + "px";
+      head.style.left = (getPosition(head, "left")+20) + "px";
       break;
     case 40: // Baixo
-      snake[0].style.top = (getPosition(snake[0], "top")+20) + "px";
+      head.style.top = (getPosition(head, "top")+20) + "px";
       break;
   }
 }
 
-function createPiece(){
+function createRandomPiece(left, top){
   if(!pieceOn){
-    let left = Math.floor(Math.random() * 9) * 20;
-    let top = Math.floor(Math.random() * 9) * 40;
-    piece = document.createElement("div");
-    piece.className = "piece";
-    piece.id = snake.length
-    piece.style.left = left+"px";
-    piece.style.top = top+"px";
-    field.insertBefore(piece, snake[0]);
+    piece = newPiece(left, top);
     pieceOn = true;
   }
 }
 
 function gameOver(){
-  let left = getPosition(snake[0], "left");
-  let top = getPosition(snake[0], "top");
+  let left = getPosition(head, "left");
+  let top = getPosition(head, "top");
   if(left < 0 || left > 180 || top < 0 || top > 380){
-    clearInterval(timer);
-    alert("Game Over");
+    return endGame();
   }
   else{
     snake.forEach(function(piece, index, array){
       if(index > 1){
-        if(colision(snake[0], piece)){
-          clearInterval(timer);
-          alert("Game Over");
+        if(colision(head, piece)){
+          return endGame();
         }
       }
     });
   }
 }
 
-function getPiece(){
-  if(colision(snake[0], piece)){
+function eatPiece(){
+  if(colision(head, piece)){
     pieceOn = false;
-    // $(piece).remove();
     piece.className = "piece tail";
     snake.push(piece);
 
+    points.innerText = snake.length-1; // Atualiza os pontos
+
+    // Acelera o jogo em 10 milisegundos
     clearInterval(timer);
     time -= 10
     timer = setInterval(loop, time);
@@ -96,26 +83,11 @@ function refreshTail(){
       if(array.length > index+1){
         piece.style.left = getPosition(array[index+1], "left")+"px";
         piece.style.top = getPosition(array[index+1], "top")+"px";
-        // $(piece).css({left: getPosition(array[index+1], "left")+"px", top: getPosition(array[index+1], "top")+"px"})
       }
       else{
-        piece.style.left = getPosition(snake[0], "left")+"px";
-        piece.style.top = getPosition(snake[0], "top")+"px";
-        // $(piece).css({left: getPosition(snake[0], "left")+"px", top: getPosition(snake[0], "top")+"px"})
+        piece.style.left = getPosition(head, "left")+"px";
+        piece.style.top = getPosition(head, "top")+"px";
       }
     }
   });
-}
-
-function colision(objA, objB){
-  let aLeft = getPosition(objA, "left");
-  let aTop = getPosition(objA, "top");
-  let bLeft = getPosition(objB, "left");
-  let bTop = getPosition(objB, "top");
-  
-  return (aLeft == bLeft && aTop == bTop)
-}
-
-function getPosition(obj, direction){  
-  return parseInt(obj.style[direction])
 }
